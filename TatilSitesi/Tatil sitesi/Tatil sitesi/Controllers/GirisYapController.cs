ba1182caf006ed20d70f3f00c0d939b7db71a053
Rumeysa Bıçakcıgil <rmysbckcgl4248@gmail.com>
2023-01-27 20:21:23 +0300

    Tatil Bloğu
    Kendi görüşlerimi paylaşabileceğim tatil bloğu havasında web sitem

---
+++ TatilSitesi/Tatil sitesi/Tatil sitesi/Controllers/GirisYapController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Tatil_sitesi.Models.Siniflar;
+
+namespace Tatil_sitesi.Controllers
+{
+    public class GirisYapController : Controller
+    {
+        // GET: GirisYap
+        Context c = new Context();
+        private object formsAuthentication;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin ad)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(X => X.Kullanici == ad.Kullanici && X.Sifre == ad.Sifre);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanıcı"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "GirisYap");
+        }
+    }
+}
---
+++ TatilSitesi/Tatil sitesi/Tatil sitesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tatil_sitesi.MvcApplication" Language="C#" %>
